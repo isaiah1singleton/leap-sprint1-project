ad5fd713e8d6e4c286221e7edd8dc9ebdd4b8bea
Lia Fitzpatrick <lfitzpatrick128@gmail.com>
2026-09-23 17:57:18 +0000

    added enums

---
+++ backend/src/main/java/com/neueda/leap/enums/AccountStatus.java
@@ -0,0 +1,6 @@
+package com.neueda.leap.enums;
+
+public enum AccountStatus {
+    ACTIVE,
+    INACTIVE
+}
---
+++ backend/src/main/java/com/neueda/leap/enums/ClientStatus.java
@@ -0,0 +1,6 @@
+package com.neueda.leap.enums;
+
+public enum ClientStatus {
+    ACTIVE,
+    INACTIVE
+}
